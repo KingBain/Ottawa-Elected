281b74b7140937a27b79fbfc4eac0a374e6fb09d
John <bain@John-HP-Compaq-6710b-RM407UA-ABA.(none)>
2010-10-31 12:41:53 -0400

    template change2

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,11 +1,15 @@
 class ViewerController < ApplicationController
 def index
 	@page = Page.find_by_name("Home")
-puts "test"
 end  
 
 def show
 	@page = Page.find_by_name(params[:name])
-  end
+	if params[:name] = "Councilors"
+	@candidates = Candidate.all
+	end
+
+end
+
 
 end
